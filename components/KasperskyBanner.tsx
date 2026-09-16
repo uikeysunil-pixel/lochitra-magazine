eb66bb7d23172c3f540e52a1e3c86ec008537c09
Sunil Kumar Uikey <uikeysunil@gmail.com>
2026-09-16 13:13:34 +0530

    Add Kaspersky CJ affiliate integration

---
+++ components/KasperskyBanner.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+export default function KasperskyBanner() {
+  return (
+    <div className="not-prose my-6 flex flex-col items-start">
+      <a
+        href="https://www.dpbolvw.net/click-101881933-17338597?sid=locitra-kaspersky"
+        target="_top"
+        rel="noopener noreferrer sponsored"
+        className="inline-block transition-opacity hover:opacity-90"
+        aria-label="Kaspersky security solutions"
+      >
+        <img
+          src="https://www.ttqklg.com/image-101881933-17338597"
+          width={150}
+          height={40}
+          alt=""
+          border={0}
+        />
+      </a>
+    </div>
+  )
+}
--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -6,6 +6,7 @@ import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import AffiliateDisclosure from './AffiliateDisclosure'
+import KasperskyBanner from './KasperskyBanner'
 
 export const components: MDXComponents = {
   Image,
@@ -16,4 +17,5 @@ export const components: MDXComponents = {
   BlogNewsletterForm,
   // ── Custom Locitra components ────────────────────────────────────────────
   AffiliateDisclosure,
+  KasperskyBanner,
 }

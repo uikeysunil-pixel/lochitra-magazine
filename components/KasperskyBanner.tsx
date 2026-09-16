c23d9861640fe4a63eeacf0af2f73ba681df8e1f
Sunil Kumar Uikey <uikeysunil@gmail.com>
2026-09-16 14:48:25 +0530

    Update Kaspersky CJ creative URLs

--- components/KasperskyBanner.tsx
+++ components/KasperskyBanner.tsx
@@ -5,13 +5,18 @@ export default function KasperskyBanner() {
   return (
     <div className="not-prose my-6 flex flex-col items-start">
       <a
-        href="https://www.dpbolvw.net/click-101881933-17338597?sid=locitra-kaspersky"
+        href="https://www.jdoqocy.com/click-101881933-17338597?sid=locitra-kaspersky"
         target="_top"
         rel="noopener noreferrer sponsored"
         className="inline-block transition-opacity hover:opacity-90"
         aria-label="Kaspersky security solutions"
       >
-        <img src="https://www.ttqklg.com/image-101881933-17338597" width={150} height={40} alt="" />
+        <img
+          src="https://www.lduhtrp.net/image-101881933-17338597"
+          width={150}
+          height={40}
+          alt=""
+        />
       </a>
     </div>
   )
